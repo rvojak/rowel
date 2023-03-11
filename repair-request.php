1545d478d2d4b59577d415aa1eb047a0f17a6251
rvojak <41307911+rvojak@users.noreply.github.com>
2023-03-11 12:45:01 +0100

    Add files via upload

---
+++ repair-request.php
@@ -0,0 +1,122 @@
+<html>
+<script src='https://www.google.com/recaptcha/api.js' async defer></script>
+<head>
+<link rel="stylesheet" href="normal.css" type="text/css">
+<title>CTPC - Repair request form</title>
+</head>
+<body>
+<div id='container'>
+<br />
+<div class='heading'><a href='https://ctpc.biz'><img style='border:0' src='ctpc-banner-2018.png'></a></div>
+
+<div style='text-align:center; font-size: 14pt; padding:10px;'><b>CTPC REPAIR REQUEST FORM</b></div>
+<div style='text-align:center;font-size:10pt;'>Unsure about something? Check the <a target='ctpcfaq' href='repair-faq.php'>FAQ</a></div>
+<br />
+
+<div class='tandcdiv'>
+<div style='text-align:center'>Terms &amp; Conditions</div>
+<ol class='tandc'>
+	<li class='tandc'>I authorize CTPC and its affiliates to perform repair work on my electronic device. I understand that CTPC or its affiliates are independent repair agents and not authorized service dealers or providers.</li> 
+	<li class='tandc'>I agree to release, indemnify, and hold CTPC and its affiliates from liability for any claims or damages of any kind or description that may arise from any repair work performed on my electronic device.</li>
+
+	<li class='tandc'>I acknowledge the device I am handing in for repair is my device &amp; not stolen property. I accept all responsiblity for the history of this device. If the device is found to be stolen, I accept that I may not receive it back.</li>
+
+	<li class='tandc'>If a device requires detailed disassembly for a quote a minimum charge of $33 will apply. This cost can be subtracted from the final cost of the repair, if you choose to go ahead. This amount is not refundable.</li>
+
+	<li class='tandc'>Device Damage
+	<ol class='tandc'>
+		<li class='tandc'>Shock and crush damaged devices: Microscopic fractures can occur that can cause issues with your device. In some cases when these fractures are present, and the mounting screws are removed, the fractures do not make connection when the device is reassembled. You agree that CTPC is not liable if fractures have occurred inside your device, which causes your device not to function in some way, even if such things were functional prior to disassembly.</li>
+
+		<li class='tandc'>Liquid damaged devices: The recovery time can vary depending on the device, and the extent of the damage. All parts that are replaced with new parts supplied by us, will be covered by the warranty period indicated on the final job sheet. This warranty period does not cover any main internals, or any parts that have not been replaced with new parts. Progressive damage can occur over time. CTPC is not responsible if your device does not function in some way, or at all, after liquid damage recovery. Liquid damage recovery will take a minimum of 24 hours, but in some cases, can take over a week. This is device/situation specific, and cannot be predicted. In some cases, displacing liquid damage can cause the device not to function in some way (Sometimes, the liquid&#39;s impurities is the only thing holding a circuit together), in such cases, you agree that CTPC is not liable.</li>
+
+		<li class='tandc'>Virus / Infected devices:  Removal can take a number of days. CTPC is not liable if your device becomes infected with a virus after a virus removal or full clean-up. We cannot future proof your computer or stop others from using it. This is the case regardless of what antivirus software you use.</li>
+
+
+		<li class='tandc'>DOA devices: 'Dead On Arrival'; No Picture, Flat Battery, Not turning on, or has a serious fault. The device cannot be fully tested until after the repair has been completed. CTPC will not be liable if any function of the device does not work partially, or in whole. eg, if your device does not turn on upon arrival to our store, and after it is repaired, the sound does not work, the issue would NOT be warranty. The policy applies to anything unable to be fully tested at the time of the Service Booking (including things that could not be tested because the customer had not supplied passwords and/or has supplied incorrect details).</li>
+		</ol>
+		</li>
+
+	<li class='tandc'>Repair
+	<ol class='tandc'>
+		<li class='tandc'>If parts need to be ordered the repair time can vary. We do not have control over postage delays that prevent a job from being completed. If anything should be lost in transit we will attempt to order it again at our discretion. Special order items may require a deposit. Deposits are not refundable.</li>
+
+		<li class='tandc'>Most types of glass are held in place by double sided adhesive. Should the glass/screen lift/detach in any way, please return it to us straight away. We will not cover warranty on a screen that is in some way broken, even if the break occurred after the screen started lifting. If the devices frame is warped/bent/twisted etc. We will do our very best to reshape the frame to get the glass/screen to sit flush. If the screen is not 100% flush because the frame is slightly or severely bent or twisted, you agree that this is not a warranty issue, and that replacing the frame of your device is the only way to get it 100% straight. CTPC will not be held liable if your screen is lifting due to a bent frame.</li>
+
+		<li class='tandc'>I understand if I am having a part replaced inside my device, that if my screen or any glass/plastic components on or in my phone are damaged, that this will affect the strength of these parts &amp; my device. These parts may damage further as a course of the repair and nececssary force applied by the technician. I do not hold CTPC or its affiliates liable if these parts are further damaged then what they were when I handed the device over for repair because their structural integrity was already compromised and the force applied was neccessary in order to complete the repair that I requested.</li>
+		</ol>
+		</li>
+
+	<li class='tandc'>Data
+	<ol class='tandc'>
+		<li class='tandc'>I understand that CTPC or its affiliates are not responsible for any data loss. I understand that I am responsible for backing up the data on my device before submitting for repair.  In the event of any data loss and hardware or software failure I do not hold CTPC or its affiliates responsible or liable for any consequences or losses arising from the loss of data.</li>
+		<li class='tandc'>I understand that CTPC and its affilaites will not browse through any personal, private or confidential information or data unless directed to or is required for verification of the repair.</li>
+		<li class='tandc'>Technicians may inadvertently see data during the course of their work. I understand that any confidential data should be removed from the device prior to having repair work performed.</li>
+		</ol>
+		</li>
+
+
+
+	<li class='tandc'>Warranties
+	<ol class='tandc'>
+		<li class='tandc'>Manufacturer&rsquo;s Warranty may be VOID upon opening a device. You accept that CTPC may break a Warranty seal upon opening your device. It is the customer&rsquo;s responsibility to determine if the device is currently under some form of warranty. If you are unsure, please determine this before we start any repairs.</li>
+		<li class='tandc'>I understand that repairs or technical support performed by CTPC or its affiliates may void manufacturer warranties. CTPC and its affiliates do not assume any liability or warranty in the event that the manufacturer warranties are voided but may, at their own sole discretion, offer their own warranty on the parts and/or services performed. Warranty offered by CTPC or its affiliates will be void if you smash, damage, break, crack or liquid damage your device.</li>
+		<li class='tandc'>CTPC Warranty is VOID if your device is opened, or serviced, by anyone other than CTPC.</li>
+		<li class='tandc'>CTPC Warranty is VOID if your device sustains any form of damage after leaving store.</li>
+		<li class='tandc'>NO Warranty is supplied for parts that were not supplied by CTPC.</li>
+		<li class='tandc'>CTPC is not responsible if your device fails in any way in our store or after if has left, unless the part that failed was supplied by us, and is under warranty.</li>
+		<li class='tandc'>If the device fails, and you believe it may be covered by warranty, you MUST return it within that period. There will be no exceptions.</li>
+		<li class='tandc'>CTPC Warranty covers only the work performed for the declared fault the device is submitted for.  Undeclared faults are not covered.</li>
+		<li class='tandc'>CTPC Warranty does not cover new faults occuring after the device has been returned.</li>
+		</ol>
+		</li>
+
+	<li class='tandc'>All Jobs must be collected within 30 days of completion. Jobs not collected in this period will be deemed as &ldquo;Uncollected Goods&rdquo; (Uncollected Goods Act 1995), and/or may incur a storage fee. Uncollected Goods may be sold/disposed of after the required time has elapsed, in accordance with &ldquo;Australia Consumer Law and Fair Trading Act 2012&rdquo; (*Sections 60, 61, 62) &amp; Uncollected Goods Act 1995.</li>
+
+	<li class='tandc'>CTPC does not offer any form of credit. Your device(s) will not leave the store unless payment has been received in full. In the event of bank reversal, or other payment complications, you will be obligated to pay CTPC the full amount plus any fees incurred immediately. Cheques are not accepted.</li>
+
+	<li class='tandc'>By submitting this form, you agree to the above terms and conditions, if you do not agree to the above terms your job will not be accepted.</li>
+
+	<li class='tandc'>These terms and conditions can change at any time, without notice.</li>
+
+	</ol>
+	</div><!-- tandcdiv -->
+
+<form name='repairrequest' action='submit-repair-request.php' method='post'>
+<table>
+<tr><td>&nbsp;</td><td><div>* = signifies <em>required</em> field</div></td></tr>
+<tr><td>&nbsp;</td><td><B>ADDRESS DETAILS</b></td></tr>
+<tr><td class='rrfieldname'>*Name</td><td class='rrfield'><input required class='rrfield' type='text' value='' name='rname' size='60'></td></tr>
+<tr><td class='rrfieldname'>*Email</td><td class='rrfield'><input required class='rrfield' type='text' value='' name='remail'></td></tr>
+<tr><td class='rrfieldname'>*Mobile/Phone Contact</td><td class='rrfield'><input required class='rrfield' type='text' value='' name='rphone'></td></tr>
+<tr><td class='rrfieldname'>*Delivery/Return Address<br /><b>NOTE:</b> Your name, email and phone will be added to this.</td><td class='rrfield'>
+<textarea required name='raddress' rows='8' cols='60' maxlength='500' onfocus="if(this.value.indexOf('Be sure to VERIFY') >= 0) { this.value = ''; }">***NOTE NOTE NOTE***
+
+Be sure to VERIFY the address.
+
+Devices will be returned to this address.</textarea></td></tr>
+<tr><td>&nbsp;</td><td><br /><br /><br /><B>DEVICE DETAILS</b></td></tr>
+
+<tr><td class='rrfieldname'>Your reference (ie, #1359, mostly for businesses, internal reference, not required)</td>
+<td class='rrfield'><input name='rreference' class='rrfield' type='text' value=''></td></tr>
+<tr><td class='rrfieldname'>*Device type/description (ie, iPhone 6, Macbook A1502)</td>
+<td class='rrfield'><input name='rdevice' class='rrfield' type='text' value=''></td></tr>
+	<!--input required class='rrfield' type='text' value='' name='rdevice'></td></tr -->
+<tr><td class='rrfieldname'>Serial Number / IMEI</td><td class='rrfield'><input class='rrfield' type='text' value='' name='rserial'></td></tr>
+<tr><td class='rrfieldname'>Password/code for device (if applicable)</td><td class='rrfield'><input class='rrfield' type='text' value='' name='rpasscode'></td></tr>
+<tr><td class='rrfieldname'>*Explanation of fault</td><td class='rrfield'><textarea required value='' name='rfault' rows='5' cols='60' maxlength='5000'></textarea></td></tr>
+<tr><td class='rrfieldname'>Has this device had prior repair attempts?</td><td class='rrfield'><input class='rrfield' type='radio' value='Yes' name='rprior'>Yes <input class='rrfield' type='radio' value='no' name='rprior' checked >No</td></tr>
+
+<tr><td style='text-align:right; padding: 1em;'>Verify that you are a human</td><td>
+<div class="g-recaptcha" data-sitekey="6Ld6p54UAAAAAKIFPR44WiemATN9Z1z3aJ-Td8Bn"></div>
+</td></tr>
+
+<tr><td style='padding-right: 1em; vertical-align:top; text-align:right'>Agree to the <a href='terms-and-conditions.php' target='tandc'>terms and conditions</a>, and submit the form</td><td style='text-align:justify;' id='cbr'><input required type='checkbox' name='tandc' id='cb' onClick='button=document.getElementById("submit-button"); if (document.getElementById("cb").checked==true) { button.disabled = ""; document.getElementById("cbr").border=""; } else { button.disabled="disabled"};'>Click to agree<br /><br /><input type='submit' id='submit-button' disabled="disabled"></td></tr>
+<tr><td colspan=2>
+<div style='background: #ff5050; text-align:center; padding: 1em; margin-top:2em;'><b style='font-size:18pt; text-align:center;'>Standard minimum fee of $66 AUD applies to all jobs, and additionally return postage if required</b></div>
+</td></tr>
+</table>
+</form>
+<div style='text-align:center; padding:15px;'><a href='/'>Back home</a></div>
+</div><!-- container -->
+</body>
+</html>
